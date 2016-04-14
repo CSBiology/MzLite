cdcf190b623116ace1e6bec7ee7ff49ebe521dac
Alexander Lüdemann <alexander.luedemann@outlook.com>
2016-04-14 12:18:03 +0200

    Initial project structure

--- src/PlayGround/Program.cs
+++ src/PlayGround/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MzLite
+namespace PlayGround
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
